72defbac84848ab12db18ee222619a2ac5cb2d69
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2025-03-08 16:52:50 +0500

    fix frame.pyi:read() typing

--- aioamqp-stubs/frame.pyi
+++ aioamqp-stubs/frame.pyi
@@ -4,10 +4,11 @@ from pamqp.base import Frame
 from pamqp.body import ContentBody
 from pamqp.frame import FrameTypes
 from pamqp.header import ContentHeader
+from pamqp.heartbeat import Heartbeat
 
 DUMP_FRAMES: bool
 
 def write(writer: StreamWriter, channel: FrameTypes, encoder: int) -> None: ...
 async def read(
     reader: StreamReader,
-) -> tuple[int, Frame | ContentHeader | ContentBody | None]: ...
+) -> tuple[int, Frame | ContentHeader | ContentBody | Heartbeat | None]: ...
