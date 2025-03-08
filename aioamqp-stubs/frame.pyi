292cc1f0d803a8d94fecc7b2bb3db0727d6aef87
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2025-03-08 18:45:46 +0500

    add protocol.py stub

--- aioamqp-stubs/frame.pyi
+++ aioamqp-stubs/frame.pyi
@@ -1,14 +1,13 @@
 from asyncio import StreamReader, StreamWriter
 
-from pamqp.base import Frame
+from pamqp.base import Frame as BaseFrame
 from pamqp.body import ContentBody
 from pamqp.frame import FrameTypes
 from pamqp.header import ContentHeader
 from pamqp.heartbeat import Heartbeat
 
 DUMP_FRAMES: bool
+_Frame = BaseFrame | ContentHeader | ContentBody | Heartbeat | None
 
 def write(writer: StreamWriter, channel: FrameTypes, encoder: int) -> None: ...
-async def read(
-    reader: StreamReader,
-) -> tuple[int, Frame | ContentHeader | ContentBody | Heartbeat | None]: ...
+async def read(reader: StreamReader) -> tuple[int, _Frame]: ...
